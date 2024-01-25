f5830aa3dd3b30f6796800b25ebdd574c08cf9b7
7Haya <hayafahad46@gmail.com>
2024-01-25 10:12:37 +0300

    Initial Commit

---
+++ mc3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mc3
+//
+//  Created by Haya Fahad Alsabr on 13/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mc3/mc3App.swift
@@ -0,0 +1,17 @@
+//
+//  mc3App.swift
+//  mc3
+//
+//  Created by Haya Fahad Alsabr on 13/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct mc3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
